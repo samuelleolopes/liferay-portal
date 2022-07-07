7831b70cd485dd643abf0352b098fc209f67e718
Samuel Lopes <samuel.lopes@liferay.com>
2022-07-07 17:58:55 -0300

    LPS-157622 Report Suspicious Message for Reply Comment

--- modules/apps/questions/questions-web/src/main/resources/META-INF/resources/js/components/Comment.es.js
+++ modules/apps/questions/questions-web/src/main/resources/META-INF/resources/js/components/Comment.es.js
@@ -13,6 +13,7 @@
  */
 
 import ClayButton from '@clayui/button';
+import ClayForm, {ClayInput, ClayRadio, ClayRadioGroup} from '@clayui/form';
 import ClayIcon from '@clayui/icon';
 import ClayLabel from '@clayui/label';
 import {useMutation} from 'graphql-hooks';
@@ -30,8 +31,24 @@ export default withRouter(
 		const [showDeleteCommentModal, setShowDeleteCommentModal] = useState(
 			false
 		);
+		const [onOpenChange, setOnOpenChange] = useState(false);
 
 		const [deleteMessage] = useMutation(deleteMessageQuery);
+
+		const suspiciousCategories = [
+			{label: 'Inappropriate content', value: 0},
+			{label: 'Harassment or bullying', value: 1},
+			{label: 'Harmful or dangerous acts', value: 2},
+			{label: 'Spam or misleading', value: 3},
+			{label: 'Infringes human rights', value: 4},
+		];
+
+		const MAX_DESCRIPTION_LENGTH = 500;
+
+		const [form, setForm] = useState({
+			description: '',
+			suspiciousCategoryId: suspiciousCategories[0]?.value,
+		});
 
 		return (
 			<div className="c-my-3 questions-reply row">
@@ -79,6 +96,117 @@ export default withRouter(
 								>
 									{Liferay.Language.get('delete')}
 								</ClayButton>
+
+								<>
+									<ClayButton
+										className="btn-sm c-mr-2 c-px-2 c-py-1"
+										data-testid="mark-as-answer-button"
+										displayType="secondary"
+										onClick={() => {
+											setOnOpenChange(true);
+										}}
+									>
+										{Liferay.Language.get('report')}
+									</ClayButton>
+									<Modal
+										body={
+											<ClayForm>
+												<h2 className="mb-3">
+													Do you want to report this
+													comment?
+												</h2>
+
+												<ClayRadioGroup
+													defaultValue={
+														form.suspiciousCategoryId
+													}
+												>
+													{suspiciousCategories.map(
+														(
+															suspiciousCategory,
+															index
+														) => (
+															<ClayRadio
+																key={index}
+																label={
+																	suspiciousCategory.label
+																}
+																onClick={() =>
+																	setForm({
+																		...form,
+																		suspiciousCategoryId:
+																			suspiciousCategory.value,
+																	})
+																}
+																value={
+																	suspiciousCategory.value
+																}
+															/>
+														)
+													)}
+												</ClayRadioGroup>
+
+												<ClayForm.Group className="form-group-sm">
+													<label htmlFor="description">
+														{Liferay.Language.get(
+															'description'
+														)}
+													</label>
+
+													<ClayInput
+														component="textarea"
+														id="description"
+														maxLength={
+															MAX_DESCRIPTION_LENGTH
+														}
+														onChange={(event) =>
+															setForm({
+																...form,
+																description:
+																	event.target
+																		.value,
+															})
+														}
+														placeholder="Report Suspicious Activity"
+														value={form.description}
+													/>
+
+													<div className="d-flex form-text justify-content-end mt-1">
+														{`${
+															form?.description
+																?.length
+																? form
+																		?.description
+																		?.length
+																: 0
+														} / ${MAX_DESCRIPTION_LENGTH}`}
+													</div>
+
+													<div className="form-text">
+														Reported activity is
+														moderated by community
+														admins to determine
+														whether they violate any
+														guidelines. Accounts are
+														penalized, and serious
+														or repeated violations
+														can lead to account
+														termination.
+													</div>
+												</ClayForm.Group>
+											</ClayForm>
+										}
+										onClose={() => {
+											setOnOpenChange(false);
+										}}
+										status="warning"
+										textPrimaryButton={Liferay.Language.get(
+											'report'
+										)}
+										title="Report comment?"
+										visible={onOpenChange}
+									/>
+								</>
 
 								<ClayButton
 									className="btn-sm c-px-2 c-py-1"
