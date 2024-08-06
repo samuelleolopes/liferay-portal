8e28b80a203b7802266cce92cc070227e21cc040
Samuel Lopes <samuel.lopes@liferay.com>
2024-08-06 20:44:01 -0300

    LRSD-1626 SF

--- workspaces/liferay-customer-workspace/client-extensions/liferay-customer-custom-element/src/routes/customer-portal/containers/GenerateNewKey/utils/renewKeySubtitle.js
+++ workspaces/liferay-customer-workspace/client-extensions/liferay-customer-custom-element/src/routes/customer-portal/containers/GenerateNewKey/utils/renewKeySubtitle.js
@@ -3,11 +3,13 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-import i18n from "~/common/I18n";
+import i18n from '~/common/I18n';
 
 const getRenewKeySubtitle = (state) => {
 	const keyCount = state?.activationKeys?.length;
-	const keyType = state?.activationKeys[0]?.complimentary ? 'complimentary' : 'subscription';	
+	const keyType = state?.activationKeys[0]?.complimentary
+		? 'complimentary'
+		: 'subscription';
 	const translationKey = keyCount === 1 ? 'renew-x-x-key' : 'renew-x-x-keys';
 
 	return i18n.sub(translationKey, [keyCount, keyType]);
